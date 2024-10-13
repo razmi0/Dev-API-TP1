93bce2fa20ae9df1b40a84d829712875ccab9d4e
razmiO <thomas.cuesta31@gmail.com>
2024-10-13 12:48:56 +0200

    add Error Utils class

---
+++ utils/Error.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Utils;
+use Exception;
+
+class Error extends Exception {
+    protected $code = 0;
+    protected $message = null;
+    private $error = null;
+    private $data = [];
+
+    public function __construct($code = 0, $message = null, $error = null, $data = [])
+    {
+        $this->code = $code;
+        $this->message = $message;
+        $this->error = $error;
+        $this->data = $data;
+    }
+
+    public function setCode($code)
+    {
+        $this->code = $code;
+        return $this;
+    }
+
+   
+
+    public function setError($error)
+    {
+        $this->error = $error;
+        return $this;
+    }
+
+    public function send()
+    {
+        error_log("Message : " . $this->message . " Error : " . $this->error);
+        header("Content-Type: application/json; charset=UTF-8");
+        http_response_code($this->code);
+        echo json_encode([
+            "message" => $this->message,
+            "data" => $this->data,
+            "error" => $this->error
+        ]);
+    }
+}
