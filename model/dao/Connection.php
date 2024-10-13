c8ec471d12618f57f59314b220453d78ff13de97
razmiO <thomas.cuesta31@gmail.com>
2024-10-13 03:11:23 +0200

    Refactor Connection class and extract pdo logic out

--- model/dao/Connection.php
+++ model/dao/Connection.php
@@ -6,61 +6,35 @@ use PDO;
 use PDOException;
 
 
-/**
- * Class Connection
- *
- * This class is responsible for managing the database connection.
- * It provides methods to establish and close the connection to the database.
- *
- * @method PDO getConnection() Returns the PDO instance for database operations.
- * @method exec($query) Executes the given query and returns the result.
- */
-class Connection {
-    /**
-     * @var PDO
-     */
-    private $connection = null;
+
+class Connection
+{
+
+    public $pdo = null;
     private $host = "localhost:3306";
     private $username = "root";
     private $password = "";
     private $db_name = "db_labrest";
 
 
-    public function __construct() {
+    public function __construct()
+    {
         try {
-            $this->connection = new PDO("mysql:host=$this->host;dbname=$this->db_name", $this->username, $this->password);
-            $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            $this->connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-            $this->connection->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-        } catch (PDOException $e) {
-            http_response_code(503);
-            echo json_encode(["message" => "Service non disponible : " . $e->getMessage()]);
-        }
-    }
-
-    public function getConnection() {
-        return $this->connection;
-    }
-
-    private function closeConnection() {
-        $this->connection = null;
-    }
-
-    public function safeExecute($prepared) {
-        try {
-            $prepared->execute();
-            $result = $prepared->fetchAll();
+            $this->pdo = new PDO("mysql:host=$this->host;dbname=$this->db_name", $this->username, $this->password);
         } catch (PDOException $e) {
             http_response_code(500);
-            echo json_encode(["message" => "Erreur lors de l'envoi de la requête : " . $e->getMessage()]);
-            die();
+            error_log($e->getMessage());
+            echo json_encode(["message" => "Erreur interne : "]);
         }
-
-        $this->closeConnection();
-
-        return $result;
     }
 
+    public function getPDO()
+    {
+        return $this->pdo;
+    }
 
+    public function closeConnection()
+    {
+        $this->pdo = null;
+    }
 }
-
