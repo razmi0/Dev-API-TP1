5cb8f94fc5c87a844aa8010b36bdbdd53dd1d420
razmiO <thomas.cuesta31@gmail.com>
2024-10-14 10:42:21 +0200

    Refactor ProduitService to use class properties and constructor for instantiation

--- model/services/ProduitService.php
+++ model/services/ProduitService.php
@@ -5,19 +5,31 @@ namespace Model\Services;
 use Model\Entities\Produit as Produit;
 use Utils\Error as Error;
 
+
+
 class ProduitService
 {
+    private $error = null;
+    private $produit = null;
+    private $NAME_MAX_LENGTH = 50;
+    private $DESCRIPTION_MAX_LENGTH = 65000;
+
+    public function __construct()
+    {
+        $this->error = new Error();
+        $this->error->setLocation("model/services/ProduitService.php")->setCode(400);
+        $this->produit = new Produit();
+    }
 
     public function createProduit($content)
     {
-        $produit = new Produit();
         try {
             $this->validate($content);
-            $produit = $this->buildProduct($content);
+            $this->buildProduct($content);
         } catch (Error $error) {
             throw $error;
         }
-        return $produit;
+        return $this->produit;
     }
 
     private function validate($content)
@@ -29,49 +41,43 @@ class ProduitService
 
     private function buildProduct($content)
     {
-        $produit = new Produit();
-        $produit->setName($content->name);
-        $produit->setDescription($content->description);
-        $produit->setPrix($content->prix);
-        $produit->setDateCreation(date("Y-m-d H:i:s"));
-        return $produit;
+        $this->produit->setId($content->id);
+        $this->produit->setName($content->name);
+        $this->produit->setDescription($content->description);
+        $this->produit->setPrix($content->prix);
+        $this->produit->setDateCreation(date("Y-m-d H:i:s"));
     }
 
     private function isNotNull($content)
     {
         if (is_null($content->name)) {
-            $this->buildError("Le nom du produit est obligatoire");
+            $this->error->setMessage("Le nom du produit est obligatoire");
         } else if (is_null($content->description)) {
-            $this->buildError("La description du produit est obligatoire");
+            $this->error->setMessage("La description du produit est obligatoire");
         } else if (is_null($content->prix)) {
-            $this->buildError("Le prix du produit est obligatoire");
+            $this->error->setMessage("Le prix du produit est obligatoire");
+        } else if (is_null($content->id)) {
+            $this->error->setMessage("L'id du produit est obligatoire");
         }
     }
 
     private function isNotEmpty($content)
     {
         if ($content->name == "") {
-            $this->buildError("Le nom du produit est obligatoire");
+            $this->error->setMessage("Le nom du produit est obligatoire");
         } else if ($content->description == "") {
-            $this->buildError("La description du produit est obligatoire");
+            $this->error->setMessage("La description du produit est obligatoire");
         } else if ($content->prix == "") {
-            $this->buildError("Le prix du produit est obligatoire");
+            $this->error->setMessage("Le prix du produit est obligatoire");
         }
     }
 
     private function isNotTooLong($content)
     {
-        if (strlen($content->name) >= 50) {
-            $this->buildError("Le nom du produit est trop long, la taille maximale est de 50 caractères");
-        } else if (strlen($content->description) > 65000) {
-            $this->buildError("La description du produit est trop longue, la taile maximale est de 65000 caractères");
+        if (strlen($content->name) >= $this->NAME_MAX_LENGTH) {
+            $this->error->setMessage("Le nom du produit est trop long, la taille maximale est de 50 caractères");
+        } else if (strlen($content->description) > $this->DESCRIPTION_MAX_LENGTH) {
+            $this->error->setMessage("La description du produit est trop longue, la taile maximale est de 65000 caractères");
         }
-    }
-
-    private function buildError($message)
-    {
-        $error = new Error();
-        $error->setCode(400)->setError($message)->setLocation("model/services/ProduitService.php :: buildError");
-        throw $error;
     }
 }
