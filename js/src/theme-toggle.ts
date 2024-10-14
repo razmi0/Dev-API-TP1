18d00982044a5105e7cc27ae4364c1d95702c7b4
razmiO <thomas.cuesta31@gmail.com>
2024-10-14 23:57:42 +0200

    Add theme toggle functionality

---
+++ js/src/theme-toggle.ts
@@ -0,0 +1,15 @@
+/**
+ * Theme Toggle
+ */
+
+const themeToggleButton = document.querySelector("button[data-theme]");
+if (themeToggleButton) {
+  themeToggleButton.addEventListener("mousedown", () => {
+    let html = document.querySelector("html[data-theme]") as HTMLElement;
+    if (html) {
+      let htmlTheme = html.dataset.theme;
+      htmlTheme = htmlTheme === "light" ? "dark" : "light";
+      html.dataset.theme = htmlTheme;
+    }
+  });
+}
