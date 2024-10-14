603f799cb7d37f415358d856d8d13700a6c5324b
razmiO <thomas.cuesta31@gmail.com>
2024-10-14 23:57:26 +0200

    Refactor HTML structure and improve form labels in index.php

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
-<html>
+<html data-theme="light">
 
 <head>
     <title>API Product Interface</title>
-    <script src="./js/index.js" defer></script>
+    <script src="./js/index.js" type="module"></script>
     <link
         rel="stylesheet"
         href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.colors.min.css">
@@ -13,85 +13,156 @@
     <link
         rel="stylesheet"
         href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.colors.min.css">
-    <link
-        rel="stylesheet"
-        href="./css/index.css">
+
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/x-icon" href="https://img.icons8.com/?size=512w&id=13910&format=png">
 </head>
 
 <body>
-    <h1>API Product Interface</h1>
-    <div class="container-fluid">
-        <article class="grid">
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+        }
+
+        small {
+            color: var(--pico-color-grey-500);
+        }
+
+        label:has(~input[required]) {
+            position: relative;
+            width: fit-content;
+
+            &:after {
+                content: "*";
+                position: absolute;
+                top: 0;
+                right: -1ch;
+                color: red;
+
+            }
+        }
+    </style>
+    <header>
+        <nav class="container">
+            <ul>
+                <h1>API Product Interface</h1>
+            </ul>
+            <ul>
+                <li>
+                    <button data-theme class="outline" data-tooltip="Toggle theme" data-placement="left">
+                        <?php include("images/theme-icon.svg"); ?>
+                    </button>
+                </li>
+            </ul>
+        </nav>
+    </header>
+    <main class="container-fluid">
+
+        <!--        -->
+        <!-- CREATE -->
+        <!--        -->
+
+        <article>
             <section data-endpoint="create">
-                <h2>Create a new product</h2>
+                <header style="margin-bottom : 1rem;">
+                    <h2 style="margin-bottom: 0;">Create a new product</h2>
+                    <small>All inputs are mandatory</small>
+                </header>
                 <form>
-                    <label for="name">Name:</label>
-                    <input type="text" id="name" name="name" placeholder="Name" required>
-                    <label for="prix">Price:</label>
-                    <input type="text" id="prix" name="prix" placeholder="Prix" required>
-                    <label for="description">Description:</label>
-                    <input type="text" id="description" name="description" placeholder="Description" required>
+                    <fieldset class="grid">
+                        <div>
+                            <label for="name">Name:</label>
+                            <input type="text" id="name" name="name" placeholder="Enter name..." required>
+                        </div>
+                        <div>
+                            <label for="prix">Price:</label>
+                            <input type="text" id="prix" name="prix" placeholder="Enter price..." required>
+                        </div>
+                    </fieldset>
+                    <fieldset>
+                        <label for="description">Description:</label>
+                        <input type="text" id="description" name="description" placeholder="Enter description..." required>
+                    </fieldset>
                     <button type="button" disabled>Create this one</button>
                 </form>
             </section>
             <section>
-                <h2>Result</h2>
             </section>
         </article>
-        <article class="grid grid-read-all">
+
+        <!--        -->
+        <!-- READ   -->
+        <!--        -->
+
+        <article>
             <section data-endpoint="read">
                 <h2>List all products</h2>
                 <button type="button">List all products</button>
             </section>
             <section class="overflow-auto">
-                <h2>Result</h2>
+
             </section>
         </article>
-        <article class="grid">
+
+        <!--        -->
+        <!-- READ ONE -->
+        <!--        -->
+
+        <article>
             <section data-endpoint="read-one">
                 <h2>Find by id</h2>
                 <form>
-                    <label for="id">Enter a valid id</label>
+                    <label for="id">Identifier :</label>
                     <input type="text" id="id" name="id" placeholder="Enter an id..." required>
                     <button disabled type="button">Find this one</button>
                 </form>
             </section>
             <section>
-                <h2>Result</h2>
+
             </section>
         </article>
-        <article class="grid">
+
+        <!--        -->
+        <!-- UPDATE -->
+        <!--        -->
+
+        <article>
             <section data-endpoint="update">
                 <h2>Update a product</h2>
                 <form>
                     <label for="name">Name:</label>
-                    <input type="text" id="name" name="name" placeholder="Name" required>
-                    <label for="price">Price:</label>
-                    <input type="text" id="price" name="price" placeholder="Price" required>
+                    <input type="text" id="name" name="name" placeholder="Enter name..." required>
+                    <label for="prix">Prix:</label>
+                    <input type="text" id="prix" name="prix" placeholder="Enter price..." required>
                     <label for="stock">Description:</label>
-                    <input type="text" id="description" name="description" placeholder="Description" required>
+                    <input type="text" id="description" name="description" placeholder="Enter description..." required>
                 </form>
             </section>
             <section>
-                <h2>Result</h2>
+
             </section>
         </article>
-        <article class="grid">
+
+        <!--        -->
+        <!-- DELETE -->
+        <!--        -->
+
+        <article>
             <section data-endpoint="delete">
                 <h2>Delete a product</h2>
                 <form>
-                    <label for="id">Enter an id :</label>
+                    <label for="id">Identifier :</label>
                     <input type="text" id="id" name="id" placeholder="Enter an id ..." required>
                     <button disabled type="button">Delete this one</button>
                 </form>
             </section>
             <section>
-                <h2>Result</h2>
+
             </section>
         </article>
-    </div>
+    </main>
 </body>
 
 </html>
