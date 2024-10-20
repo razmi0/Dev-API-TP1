1676430f4287b085b6835ae86cc0dbebe0df4824
razmiO <thomas.cuesta31@gmail.com>
2024-10-20 14:31:45 +0200

    Add lire_un endpoint

--- api/v1/lire_un.php
+++ api/v1/lire_un.php
@@ -2,128 +2,53 @@
 
 require_once "../../Autoloader.php";
 
-use Model\Dao\ProduitDao as ProduitDao;
-use Utils\Response as Response;
-use Utils\Error as Error;
 
-// Instantiate the controller
-// --
-$controller = new Controller();
+use HTTP\Request;
+use HTTP\Response;
+use Controller\Controller;
+use Model\Constant;
+use Model\Dao\ProduitDao;
+use Model\Schema\Schema;
 
-// Handle the request (verify the method and process the request)
-// If the method is allowed, the DAO method "create" is called and the request is processed.
-// Else, an error message is returned.
-// --
-$controller->handleRequest();
+// Start the controller with the Request and Response objects
+$controller = new Controller(
+    ["GET"],
+    new Request([
+        "endpoint" => "/api/v1/lire_un.php",
+    ]),
+    new Response([
+        "code" => 200,
+        "message" => "Produits trouvés",
+    ]),
+    new Schema(Constant::ID_SCHEMA)
+);
 
-// Handle the response (set the headers and send the response) using the Response class
-// --
-$controller->handleResponse();
+// Handle the request with Request and Response in handleRequest closure and add the product schema
+$controller->handleRequest(function () {
 
+    // Get the id from the query
+    $idInQuery = $this->request->getQueryParam("id");
 
-class Controller
-{
-    private $produitDao = null;
-    private $response = null;
-    private $message = "";
-    private $data = [];
-    private $code = 0;
-    private $error = null;
+    // Get the id from the body
+    $idInBody = $this->request->getClientDecodedData("id");
 
-    public function __construct()
-    {
-        $this->produitDao = new ProduitDao();
-        $this->response = new Response();
-        $this->error = new Error();
-        $this->error->setLocation("api/v1/lire_un.php");
+    // If the id is not present in the query or in the body, throw an error
+    if (!$idInQuery && !$idInBody) {
+        $error_message = "Veuillez envoyer l'id du produit à rechercher. Vous pouvez envoyer l'id par le corps de la requête ou par l'url. L'id par l'url est prioritaire.";
+        throw $this->error
+            ->setCode(400)
+            ->setError("Requête invalide")
+            ->setMessage($error_message);
     }
 
-    /**
-     * 
-     * Handle the request
-     * 
-     */
-    public function handleRequest()
-    {
-        switch ($_SERVER["REQUEST_METHOD"]) {
+    // Get the id and cast it to an integer
+    $id = $idInQuery  ? (int)$idInQuery : (int)$idInBody;
 
-                /**
-             * GET request : /api/v1/lire_un.php
-             * Seulement les requêtes GET sont autorisées.
-             */
-            case "GET":
-                try {
-                    // We get all the parameters from the URI ( path and query )
-                    // --
-                    $urlParams = parse_url($_SERVER["REQUEST_URI"]);
+    // Start the DAO
+    $produitDao = new ProduitDao();
 
-                    // We check if the query is present in the URI
-                    // --
-                    $hasQuery = isset($urlParams["query"]);
+    // Get the product from the database
+    $product = $produitDao->findById($id);
 
-                    // We get the id from the query or the body of the request
-                    // If no id is found, id will be null and an error will be returned.
-                    // --
-                    if ($hasQuery) {
-                        parse_str($urlParams["query"], $params);
-                        $id = isset($params["id"]) ? $params["id"] : null;
-                    } else {
-                        $client_json = json_decode(file_get_contents("php://input"));
-                        $id = isset($client_json->id) ? $client_json->id : null;
-                    }
-
-                    // If no id is found, we return an error
-                    // --
-                    if (!isset($id)) {
-                        $this->error->setCode(400)
-                            ->setError("Requête invalide")
-                            ->setMessage("Veuillez envoyer l'id du produit à rechercher. Vous pouvez envoyer l'id par le corps de la requête ou par l'url. L'id par l'url est prioritaire.")
-                            ->sendAndDie();
-                    }
-
-                    // We call the DAO method "findById" to get the product
-                    // If the request is not successful, we handle the error with the Error class.
-                    // --
-
-                    $this->data = $this->produitDao->findById($id) ?? [];
-                    $this->message = "Produit trouvé";
-                    $this->code = 200;
-                } catch (Error $e) {
-                    $e->sendAndDie();
-                }
-
-                break;
-
-                /**
-                 * Autres requêtes
-                 * Une erreur 405 et une erreur est retournée.
-                 */
-            default:
-                $error = new Error();
-                $error->setCode(405)
-                    ->setError("Methode non autorisée")
-                    ->setMessage("Veuillez utiliser la méthode GET pour obtenir un produit. Vous pouvez envoyer l'id par le corps de la requête ou par l'url. L'id par l'url est prioritaire.")
-                    ->sendAndDie();
-                break;
-        }
-    }
-
-    /**
-     * Handle the response
-     */
-    public function handleResponse()
-    {
-        $headers = [
-            "Content-Type: application/json",
-            "Access-Control-Allow-Origin: *",
-            "Access-Control-Allow-Methods: GET",
-            "Access-Control-Age: 3600",
-            "Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With"
-        ];
-        $this->response->setCode($this->code)
-            ->setData($this->data)
-            ->setMessage($this->message)
-            ->setHeaders($headers)
-            ->sendAndDie();
-    }
-}
+    return ["product" => $product];
+});
