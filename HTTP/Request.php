b4533275737d9f84364ad362fd415a681ab3ebbf
razmiO <thomas.cuesta31@gmail.com>
2024-10-20 14:30:39 +0200

    Add more protected props from $_SERVER

--- HTTP/Request.php
+++ HTTP/Request.php
@@ -36,28 +36,92 @@ namespace HTTP;
 class Request
 {
 
+    // Request & Response related properties
     protected string $request_method = "";
     protected array $authorized_methods = [];
     protected string $endpoint = "";
+
+    // Client data related properties
     protected string $client_raw_json = "";
     protected array $client_decoded_data = [];
+    protected bool $has_data = false;
     protected bool $valid_json = true;
     protected string $json_error_msg = "";
 
+    // URI related properties
+    // flags
+    protected bool $has_scheme = false;
+    protected bool $has_host = false;
+    protected bool $has_port = false;
+    protected bool $has_path = false;
+    protected bool $has_query = false;
+
+    // URI components
+    protected ?string $scheme = "";
+    protected ?string $host = "";
+    protected ?string $port = "";
+    protected ?string $path = "";
+    protected ?string $query = "";
+
+    // URI parameters
+    protected array $url_params = [];
+    protected array $query_params = [];
+
     public function __construct(array $request)
     {
-        $this->authorized_methods = $request["methods"] ?? [];
+        // Get the request method (GET, POST, PUT, DELETE, ...)
         $this->request_method = $_SERVER["REQUEST_METHOD"] ?? "";
+
+        // Get the authorized methods
+        $this->authorized_methods = $request["methods"] ?? [];
+
+        // Get the endpoint
         $this->endpoint = $request["endpoint"] ?? "";
+
+        // Get the URI parameters
+        $this->url_params = parse_url($_SERVER["REQUEST_URI"]);
+
+        // Assign URI components to class properties
+
+        // build the URI parameters
+        [
+            $this->scheme,
+            $this->host,
+            $this->port,
+            $this->path,
+            $this->query,
+        ] = $this->buildURIParams();
+
+        // build the URI flags
+        [
+            $this->has_scheme,
+            $this->has_host,
+            $this->has_port,
+            $this->has_path,
+            $this->has_query,
+        ] = $this->buildURIFlags();
+
+        // Parse the query parameters for easy access into associative array (key => value)
+        if ($this->has_query) {
+            parse_str($this->query, $this->query_params);
+        }
+
+        // Get the client data
         $this->client_raw_json = file_get_contents("php://input") ?? "";
+
+        // Decode the client data
+        // if no data is present, set the decoded data to an empty array
         $this->client_decoded_data = json_decode($this->client_raw_json, true) ?? [];
 
+        // Check if there was an error decoding the JSON
         if (!empty($this->client_decoded_data)) {
-            $this->json_error_msg = json_last_error_msg();
 
-            if ($this->json_error_msg !== "No error") {
-                $this->valid_json = false;
-            }
+            // Data is present
+            $this->has_data = true;
+
+            // Check if the JSON is valid
+            $this->json_error_msg = json_last_error_msg();
+            $this->valid_json = ($this->json_error_msg === "No error");
         }
     }
 
@@ -80,8 +144,11 @@ class Request
     {
         return $this->endpoint;
     }
-    public function getClientDecodedData(): array
+    public function getClientDecodedData(string $key = null): mixed
     {
+        if ($key) {
+            return $this->client_decoded_data[$key] ?? [];
+        }
         return $this->client_decoded_data;
     }
     public function getIsValidJson(): bool
@@ -95,5 +162,50 @@ class Request
     public function getClientRawJson(): string
     {
         return $this->client_raw_json;
+    }
+    public function getHasData(): bool
+    {
+        return $this->has_data;
+    }
+
+    private function buildURIParams(): array
+    {
+        return [ // http, https
+            empty($_SERVER['HTTPS']) ? 'http' : 'https',
+            // localhost
+            $_SERVER["HTTP_HOST"] ?? null,
+            // 80
+            $_SERVER['SERVER_PORT'] ?? null,
+            // /api/v1/{endpoint}.php
+            $this->url_params['path'] ?? null,
+            // ?param1=1?param2=2
+            $this->url_params['query'] ?? null,
+        ];
+    }
+
+    private function buildURIFlags(): array
+    {
+        $keys = ["scheme", "host", "port", "path", "query"];
+        return array_map(fn($key) => isset($this->$key), $keys);
+    }
+
+    public function getQueryParam($key): string
+    {
+        return $this->query_params[$key] ?? "";
+    }
+
+    public function getPath(): string
+    {
+        return $this->path;
+    }
+
+    public function getFlag($flag): bool
+    {
+        return $this->$flag;
+    }
+
+    public function getURIComponent($key): string
+    {
+        return $this->$key;
     }
 }
