efa5b5005058e51757dd9c32371fc9382a1b1de6
razmiO <thomas.cuesta31@gmail.com>
2024-10-24 16:41:26 +0200

    fix :  schema setting a field in client as not required and key in client no provided particular case

--- model/schema/Schema.php
+++ model/schema/Schema.php
@@ -62,7 +62,9 @@ use Exception as Exception;
  * 
  * Class Schema
  * 
- * A schema is a set of rules that the client data must follow. Provided with a valid Template, the Schema class can parse the client data and validate it against the rules explicitly defined in the schema by the consumer.
+ * A schema is a set of rules that the client data must follow.
+ * Provided with a valid Template, the Schema class can parse the client data and validate it
+ * against the rules explicitly defined in the schema by the consumer.
  * 
  * @package Schema
  * @author Cuesta Thomas
@@ -70,14 +72,10 @@ use Exception as Exception;
  * 
  * <code>
  * <?php
- * $template = Template::fromArray($arrayOfRules);
  * $schema = new Schema($template);
  * $allResults = $schema->safeParse($clientJson)->getResults();
  * ?>
  * </code>
- * 
- * @todo Decouple json_decode from the class so the input can be any type of php array.
- * 
  * 
  */
 class Schema extends SchemaCore
@@ -85,114 +83,68 @@ class Schema extends SchemaCore
 
     /**
      * 
-     * 
      * Stores all the results of the client data validation against the schema after parsing.
      * It is the final output of the parsing and validation process.
      * ( valid and not valid results )
-     * 
      * 
      */
     private ValidatorResult | array $results = [];
     /**
      * 
-     * 
-     * 
      * Stores only the error results.
-     * 
-     * 
-     * 
      * 
      */
     private ValidatorResult | array $errorResults = [];
     /**
      * 
-     * 
-     * 
-     * 
      * Stores only the success results.
-     * 
-     * 
      * 
      */
     private ValidatorResult | array $successResults = [];
     /**
      * 
-     * 
-     * 
-     * 
      * Stores the schema definition.
-     * 
-     * 
      * 
      */
     private array $schema = [];
     /**
      * 
-     * 
-     * 
-     * 
      * Stores all the rules for each key in the schema.
      * Internally managed.
      * The client data will be validated against these rules.
-     * 
      * 
      */
     private array $validationMap = [];
     /**
      * 
-     * 
-     * 
-     * 
      * Indicates whether the schema is defined.
-     * 
-     * 
      * 
      */
     private bool $hasSchema = false;
     /**
      * 
-     * 
-     * 
-     * 
      * Indicates whether the schema has been processed and the validation map has been built.
      * Avoid processing the schema multiple times.
      * Internally managed.
-     * 
      * 
      */
     private bool $isProcessed = false;
     /**
      * 
-     * 
-     * 
-     * 
      * Indicates whether the schema has been parsed.
-     * 
-     * 
      * 
      */
     private bool $isParsed = false;
     /**
      * 
-     * 
-     * 
-     * 
      * Indicates whether there are any errors in the client data.
-     * 
-     * 
      * 
      */
     private bool $hasError = false;
     /**
      * 
-     * 
-     * 
      * The constructor cast the template to a schema.
      * A schema is nothing else than a valid template.
-     * @param Template $template
-     * 
-     * 
-     * 
      * 
      */
     public function __construct(array $schema)
@@ -204,16 +156,10 @@ class Schema extends SchemaCore
 
     /**
      * 
-     * 
-     * 
-     * 
      * Resets the schema validation and processing but not the schema definition.
      * Use it to reset the validation process and start over with the same schema.
      * Throw an exception if the schema is not set.
      * @throw Exception
-     * 
-     * 
-     * 
      * 
      */
     public function resetParsing(): void
@@ -270,16 +216,8 @@ class Schema extends SchemaCore
 
     /**
      * 
-     * 
-     * 
-     * 
      * Builds the validation map from the schema.
      * Insert corrensponding rules for each key in the schema calling the Core class.
-     * @todo Extract this method to Core ?
-     * 
-     * 
-     * 
-     * 
      * 
      */
     private function processSchema(): void
@@ -287,15 +225,10 @@ class Schema extends SchemaCore
         // Loop through the valid schema provided by the consumer
         // --
         foreach ($this->schema as $key => $value) {
+            $type = $value["type"];
 
             // Loop through the rules provided by the consumer
             // --
-            $type = $value["type"];
-
-            if (self::in($value, "required") && self::in($value, "optional")) {
-                throw new Exception("You can't set both required and optional for the same key.");
-            }
-
             foreach ($value as $constraint => $constraintValue) {
 
                 // Process the rules and store them in the validationMap
@@ -321,7 +254,6 @@ class Schema extends SchemaCore
             }
         }
 
-
         // Mark the schema as processed
         // --
         $this->isProcessed = true;
@@ -330,17 +262,11 @@ class Schema extends SchemaCore
 
     /**
      * 
-     * 
-     * 
-     * 
      * Parse the client data against the validationMap and throw exceptions stopping the execution.
      * @param string $clientJson
      * @throws Exception
      * @throws ValidatorError
      * @return Schema
-     * 
-     * 
-     * 
      * 
      */
     public function parse(string $clientJson): Schema
@@ -361,10 +287,18 @@ class Schema extends SchemaCore
         // --
         $this->processSchema();
 
+
+
         try {
+
+
+
             // Decode the client data
             // --
             $clientData = json_decode($clientJson, true);
+
+
+
 
             // Loop through the keys provided in the json data 
             // The validationMap keys and the client data keys match
@@ -462,16 +396,43 @@ class Schema extends SchemaCore
         // --
         $this->processSchema();
 
+        // There's a particular case where the schema provide a key that is not in the client data
+        // if the schema set that this key is not required ("required" => false), we don't need to validate it because the consumer doesn't require
+        // this key to be present in the client data.
+        // Note : if no "required" key is set in the schema, all keys are required by default.
+
+        $notRequiredKeys = array_filter(array_map(function ($key, $value) {
+            if (array_key_exists("required", $value) && $value["required"] === false) {
+                return $key;
+            }
+        }, array_keys($this->schema), $this->schema), fn($value) => $value !== null);
+
         // Loop through the keys provided in the json data 
         // The validationMap keys and the client data keys match
         // --
         foreach ($this->validationMap as $key => $rules) {
 
 
+            // Here we handle the particular case where the key is not required and the client data doesn't have this key
+            // --
+            if (!array_key_exists($key, $clientJson) && in_array($key, $notRequiredKeys)) {
+                // we only store the required rule in the results so we need to strip the other rules
+                // --
+                $required_rule = array_filter($rules, fn($rule) => $rule->getRule() === "required");
+                $validated = $required_rule[0]->validate($this->schema[$key], $key);
+                $readable = $validated->getReadable();
+                $this->results[] = $readable;
+                $this->successResults[] = $readable;
+                continue;
+            }
+
 
             // Loop through the rules for each key of data
             // --
             foreach ($rules as $rule) {
+
+
+
 
                 // Validate the data against the rule and store the result as a ValidatorResult object
                 // --
@@ -516,11 +477,5 @@ class Schema extends SchemaCore
         $this->isParsed = true;
 
         return $this;
-    }
-
-
-    static function in(array $array, $value): bool
-    {
-        return array_key_exists($value, $array);
     }
 }
