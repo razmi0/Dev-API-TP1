2ba629193826c80472b18d69582ff44499828acc
razmiO <thomas.cuesta31@gmail.com>
2024-10-24 15:03:45 +0200

    Refactor lire endpoint with new Controller structure

--- api/v1/lire.php
+++ api/v1/lire.php
@@ -2,33 +2,42 @@
 
 require_once "../../Autoloader.php";
 
-
 use HTTP\Request;
 use HTTP\Response;
+use Model\Constant;
+use Model\Schema\Schema;
 use Controller\Controller;
 use Model\Dao\ProduitDao;
+use Model\Entities\Produit;
 
 // Start the controller with the Request and Response objects
-$controller = new Controller(
-    ["GET"],
+new Controller(
     new Request([
+        "methods" => ["GET"],
         "endpoint" => "/api/v1/lire.php",
     ]),
     new Response([
         "code" => 200,
         "message" => "Produits récupérés avec succès",
-    ])
+    ]),
+    // No client data expected on this endpoint so no schema validation step is needed
+    null,
+    function () {
+
+        // Create a new ProduitDao object
+        $produitDao = new ProduitDao();
+
+        // Get all products from the database
+        /**
+         * @var Produit[] $allProducts
+         */
+        $allProducts = $produitDao->findAll();
+
+        // Map the products to an array
+        $productsArray = array_map(fn($product) => $product->toArray(), $allProducts);
+
+        // Return the inserted ID
+        return ["products" => $productsArray];
+    }
+
 );
-
-// Handle the request with Request and Response in handleRequest closure and add the product schema
-$controller->handleRequest(function () {
-
-    // Create a new ProduitDao object
-    $produitDao = new ProduitDao();
-
-    // Catch the inserted ID from the database
-    $allProducts = $produitDao->findAll();
-
-    // Return the inserted ID
-    return ["products" => $allProducts];
-});
