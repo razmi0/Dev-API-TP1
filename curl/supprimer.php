1950f505ceb8ccca5d493f47f279a47803bbcd92
razmiO <thomas.cuesta31@gmail.com>
2024-10-25 21:47:58 +0200

    Add supprimer curl php script

--- curl/supprimer.php
+++ curl/supprimer.php
@@ -4,48 +4,38 @@
  * 
  * DELETE : /api/v1/supprimer.php endpoint 
  */
-const URL = "http://localhost/TP1/api/v1/supprimer.php";
+
+require_once "Autoloader.php";
+
+use Curl\Curl;
+
+// The endpoint
+const ENDPOINT = "delete";
+
+// The minimum and maximum id
 const MIN_ID = 1;
 const MAX_ID = 200;
 
-// Generate a random id between 1 and 200 and encode it as a json string
-$data = json_encode(["id" => (string)rand(MIN_ID, MAX_ID)]);
+// The random id
+$random_id = rand(MIN_ID, MAX_ID);
 
-// Set the headers
-$headers = [
-    'Content-Type: application/json',
-    'Content-Length: ' . strlen($data)
-];
+// The data to send
+$data = ["id" => $random_id];
 
-// Initialize a new curl session
-$ch = curl_init(URL);
+// The curl object and parameters
+$curl = new Curl(
+    [
+        "endpoint" => "delete",
+        "options" => [
+            "method" => "DELETE",
+            "data" => Curl::encodeData($data)
+        ]
+    ]
 
-// Set the options for the curl session
-// CURLOPT_CUSTOMREQUEST : The HTTP method to use
-// CURLOPT_RETURNTRANSFER : Return the response as a string
-// CURLOPT_POSTFIELDS : The data to send
-// CURLOPT_HTTPHEADER : The headers to send
-$allOptSuccess = curl_setopt_array($ch, [
-    CURLOPT_CUSTOMREQUEST => "DELETE",
-    CURLOPT_RETURNTRANSFER => true,
-    CURLOPT_POSTFIELDS => $data,
-    CURLOPT_HTTPHEADER => $headers,
-]);
+);
 
-// If one of the options failed, print the error
-if (!$allOptSuccess) {
-    echo "A cURL option was not set : " . curl_error($ch);
-}
+// Execute the curl session and close it
+$curl->executeAndClose();
 
-$response = curl_exec($ch);
-
-// If the response is false, print the error
-if ($response === false) {
-    echo "An error occur in the API" . curl_error($ch);
-}
-
-// Print the response
-var_export(json_decode($response, true));
-
-// Close the curl session
-curl_close($ch);
+// Print the stored results
+$curl->print_results();
