088d4a145a3bfb9a6eda3baa810029c07b212067
razmiO <thomas.cuesta31@gmail.com>
2024-10-25 19:48:07 +0200

    Add columns constant

--- model/Constant.php
+++ model/Constant.php
@@ -18,12 +18,7 @@ class Constant
     private const DESCRIPTION_REGEX = "/^[a-zA-Z0-9-'%,.:\/&()|; ]+$/";
     private const PRICE_REGEX = "/^[0-9.]+$/";
     private const ID_REGEX = "/^[0-9]+$/";
-    private const columns = ["id", "name", "description", "prix", "date_creation"];
-
-    public function getColumns()
-    {
-        return self::columns;
-    }
+    public const COLUMNS = ["id", "name", "description", "prix", "date_creation"];
 
     /**
      * This schema will be use in /api/v1/creer.php to validate client json
