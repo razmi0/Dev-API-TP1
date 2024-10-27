b6f283830a9e4578311051e9314d2b9adcacab77
razmiO <thomas.cuesta31@gmail.com>
2024-10-27 18:40:44 +0100

    Refacto : Improve this endpoint

--- api/v1/supprimer.php
+++ api/v1/supprimer.php
@@ -1,34 +1,88 @@
 <?php
 
+//  _____ _______ _______ _______ _______ _______ _______ _______
+// |                                                             |
+// |                DELETE PRODUCT ENDPOINT                      |
+// |                                                             |
+// |          endpoint :  [DELETE] /api/v1.0/produit/delete      |
+// |          file     :           /api/v1/supprimer.php         |
+// |          goal     :  delete one product from db             |
+// |_____________________________________________________________|
+//
+
 require_once "../../Autoloader.php";
 
 
+/**
+ * 
+ * IMPORTS
+ * 
+ */
+// HTTP classes
 use HTTP\Request;
 use HTTP\Response;
+use HTTP\Error;
+
+// Controller class
 use Controller\Controller;
+
+// Middleware class
+use Middleware\Middleware;
+
+// Model classes
 use Model\Constant;
 use Model\Dao\ProductDao;
 use Model\Schema\Schema;
-use HTTP\Error;
-use Middleware\Middleware;
 
-// Start the controller with the Request and Response objects
-$app = $controller = new Controller(
-    new Request([
-        "methods" => ["DELETE"],
-        "endpoint" => "/api/v1/supprimer.php",
-    ]),
-    new Response([
-        "code" => 200,
-        "message" => "Produit supprimé avec succès",
-    ]),
-    new Middleware([
-        "checkAllowedMethods" => [],
-        "checkValidJson" => [],
-        "checkExpectedData" => new Schema(Constant::DELETE_SCHEMA),
-    ]),
+/**
+ * 
+ * INSTRUCTIONS
+ * 
+ */
+
+// The request object is configured for DELETE requests only on this endpoint
+$request = new Request([
+    "methods" => ["DELETE"],
+]);
+
+// The response object is configured to return a 200 status code and a successfull message
+$response = new Response([
+    "code" => 200,
+    "message" => "Produit supprimé avec succès",
+]);
+
+// Controller object
+$app = new Controller($request, $response);
+
+// Middleware object
+$app->setMiddleware(
+    new Middleware(
+
+        // Context : Middleware scope and new Middleware object
+        // In this context, we access to : Request and Middleware objects
+
+        function () {
+            // Check if the request method is allowed else throw an error                ( 405 Method Not Allowed )
+            $this->checkAllowedMethods();
+
+            // Check if the request body is a valid JSON else throw an error             ( 400 Bad Request )
+            $this->checkValidJson();
+
+            // Check if the request body contains the expected data else throw an error  ( 400 Bad Request )
+            $this->checkExpectedData(new Schema(Constant::DELETE_SCHEMA));
+        }
+    )
+);
+
+// We set the handler for the controller to do the business logic
+// The handler is the core of the controller, it contains the business logic
+$app->setHandler(
+
     function () {
         // Get the id from the body
+        /**
+         * @var int $id
+         */
         $id = $this->request->getDecodedBody("id");
 
         // If the id is not present in the body, throw an error
@@ -38,10 +92,10 @@ $app = $controller = new Controller(
         }
 
         // Start the DAO
-        $ProductDao = new ProductDao();
+        $dao = new ProductDao();
 
         // Get the product from the database
-        $affectedRows = $ProductDao->deleteById($id);
+        $affectedRows = $dao->deleteById($id);
 
         // If no product was found, we send a 204 with no content in response body as HTTP specification states
         if ($affectedRows === 0) {
@@ -53,4 +107,7 @@ $app = $controller = new Controller(
     }
 );
 
+// Run the configured controller : 
+//          - Run the middlewares sequentially
+//          - Run the handler
 $app->run();
