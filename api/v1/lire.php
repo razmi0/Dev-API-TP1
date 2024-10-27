29549bf8824d3a1f7d336f67409e3079cdaa3d76
razmiO <thomas.cuesta31@gmail.com>
2024-10-27 10:51:10 +0100

    Refactor middleware handling in lire.php

--- api/v1/lire.php
+++ api/v1/lire.php
@@ -13,20 +13,32 @@ use Model\Schema\Schema;
 
 // Create a new Controller object and start it with the request, response, middleware and handler
 $app = new Controller(
+    // We expect a GET request to this endpoint
     new Request([
         "methods" => ["GET"],
         "endpoint" => "/api/v1/lire.php",
     ]),
+    // We expect to return a 200 status code and a message
     new Response([
         "code" => 200,
         "message" => "Produits récupérés avec succès",
     ]),
-    new Middleware([
-        "checkAllowedMethods" => [],
-        "checkValidJson" => [],
-        // We validate "limit" as an optional integer extending from 1 to infinity (null)
-        "checkExpectedData" => new Schema(Constant::READ_ALL),
-    ]),
+    // We will use 3 middlewares to check if the request is valid
+    new Middleware(
+        // Function context : SCOPE : Middleware (class)
+        //                    OBJECT : new Middleware ($this)
+        function () {
+            // Check if the request method is allowed else throw an error ( 405 Method Not Allowed )
+            $this->checkAllowedMethods();
+            // Check if the request body is a valid JSON else throw an error ( 400 Bad Request )
+            $this->checkValidJson();
+            // Check if the request body contains the expected data else throw an error ( 400 Bad Request )
+            $this->checkExpectedData(new Schema(Constant::READ_ALL));
+        }
+    ),
+    // Business logic at this endpoint : Get all products from the database
+    // Function context : SCOPE : Controller (class)
+    //                    OBJECT : new Controller ($this)
     function () {
 
         // Get the limit from the query parameters
@@ -35,7 +47,7 @@ $app = new Controller(
         // Get the limit from the body
         $limitInBody = $this->request->getDecodedBody("limit");
 
-        // Get the limit
+        // Get the limit and cast it to an integer
         $limit = $limitInQuery ? (int)$limitInQuery : (int)$limitInBody;
 
         // Create a new ProductDao object
