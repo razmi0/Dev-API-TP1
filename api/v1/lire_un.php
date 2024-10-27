10e0f5cda2a5a43d85781dfbd18fe16d151b2058
razmiO <thomas.cuesta31@gmail.com>
2024-10-27 18:20:22 +0100

    Refactor: Improve comment in lire_un.php to clarify the purpose of the read one product endpoint

--- api/v1/lire_un.php
+++ api/v1/lire_un.php
@@ -1,37 +1,92 @@
 <?php
 
+//  _____ _______ _______ _______ _______ _______ _______ _______
+// |                                                             |
+// |                READ ONE PRODUCT ENDPOINT                    |
+// |                                                             |
+// |          endpoint :  [GET] /api/v1.0/produit/listone        |
+// |          file     :        /api/v1/lire_un.php              |
+// |          goal     :  retrieve many products from db         |
+// |_____________________________________________________________|
+//
+
 require_once "../../Autoloader.php";
 
+/**
+ * 
+ * IMPORTS
+ * 
+ */
+
+// HTTP classes
 use HTTP\Request;
 use HTTP\Response;
 use HTTP\Error;
+
+// Controller class
 use Controller\Controller;
+
+// Middleware class
 use Middleware\Middleware;
+
+// Model classes
 use Model\Constant;
 use Model\Dao\ProductDao;
 use Model\Schema\Schema;
 
-$app = new Controller(
-    new Request([
-        "methods" => ["GET"],
-        "endpoint" => "/api/v1/lire_un.php",
-    ]),
-    new Response([
-        "code" => 200,
-        "message" => "Produit trouvé",
-    ]),
+/**
+ * 
+ * INSTRUCTIONS
+ * 
+ */
+// The request object is configured for GET requests only on this endpoint
+$request = new Request([
+    "methods" => ["GET"],
+]);
+
+// The response object is configured to return a 200 status code and a successfull message
+$response = new Response([
+    "code" => 200,
+    "message" => "Produit trouvé",
+]);
+
+// Controller object
+$app = new Controller($request, $response);
+
+
+// Middleware object
+$app->setMiddleware(
     new Middleware(
+
+        // Context : Middleware scope and new Middleware object
+        // In this context, we access to : Request and Middleware objects
+
         function () {
+            // Check if the request method is allowed else throw an error                ( 405 Method Not Allowed )
             $this->checkAllowedMethods();
+
+            // Check if the request body is a valid JSON else throw an error             ( 400 Bad Request )
             $this->checkValidJson();
+
+            // Check if the request body contains the expected data else throw an error  ( 400 Bad Request )
             $this->checkExpectedData(new Schema(Constant::READ_ONE_SCHEMA));
         }
-    ),
+    )
+);
+
+$app->setHandler(
+
     function () {
-        // Get the id from the query ( string | null )
+        // Get the id from the query
+        /**
+         * @var string $idInQuery
+         */
         $idInQuery = $this->request->getQueryParam("id");
 
-        // Get the id from the body ( integer | null )
+        // Get the id from the body
+        /**
+         * @var int $idInBody
+         */
         $idInBody = $this->request->getDecodedBody("id");
 
         // If the id is not present in the query or in the body, throw an error
@@ -56,4 +111,7 @@ $app = new Controller(
     }
 );
 
+// Run the configured controller : 
+//          - Run the middlewares sequentially
+//          - Run the handler
 $app->run();
