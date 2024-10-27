2b5d568b40fde57e9235c2306a140984e6bf585c
razmiO <thomas.cuesta31@gmail.com>
2024-10-27 18:30:25 +0100

    Refactor : Improve update endpoint

--- api/v1/modifier.php
+++ api/v1/modifier.php
@@ -1,49 +1,96 @@
 <?php
 
+//  _____ _______ _______ _______ _______ _______ _______ _______
+// |                                                             |
+// |                 UPDATE PRODUCT ENDPOINT                     |
+// |                                                             |
+// |          endpoint :  [PUT] /api/v1.0/produit/update         |
+// |          file     :        /api/v1/modifier.php             |
+// |          goal     :  update a product in db                 |
+// |_____________________________________________________________|
+//
+
 require_once "../../Autoloader.php";
 
+/**
+ * 
+ * IMPORTS
+ * 
+ */
 
+// HTTP classes
 use HTTP\Request;
 use HTTP\Response;
+
+// Controller class
+use Controller\Controller;
+
+// Middleware class
+use Middleware\Middleware;
+
+// Model classes
 use Model\Constant;
 use Model\Schema\Schema;
-use Controller\Controller;
-use Middleware\Middleware;
 use Model\Dao\ProductDao;
 use Model\Entities\Product;
 
-// Start the controller with the Request and Response objects
-$app = new Controller(
+/**
+ * 
+ * INSTRUCTIONS
+ * 
+ */
+// The request object is configured for PUT requests only on this endpoint
+$request = new Request([
+    "methods" => ["PUT"],
+]);
 
-    new Request([
-        "methods" => ["PUT"],
-        "endpoint" => "/api/v1/modifier.php",
-    ]),
-    new Response([
-        "code" => 200,
-        "message" => "Produit modifié avec succès",
-    ]),
-    new Middleware([
-        "checkAllowedMethods" => [],
-        "checkValidJson" => [],
-        "checkExpectedData" => new Schema(Constant::UPDATE_SCHEMA),
-    ]),
+// The response object is configured to return a 200 status code and a successfull message
+$response = new Response([
+    "code" => 200,
+    "message" => "Produit modifié avec succès",
+]);
+
+// Controller object
+$app = new Controller($request, $response);
+
+// Middleware object
+$app->setMiddleware(
+    new Middleware(
+        [
+            "checkAllowedMethods" => [],
+            "checkValidJson" => [],
+            "checkExpectedData" => new Schema(Constant::UPDATE_SCHEMA),
+        ]
+    )
+);
+
+// We set the handler for the controller to do the business logic
+// The handler is the core of the controller, it contains the business logic
+$app->setHandler(
+
     function () {
         // Get the client data
+        /**
+         * @var array Partial Product data
+         */
         $client_data = $this->request->getDecodedBody();
 
         // Create a new product
         $product = Product::make($client_data);
 
         // Start the DAO
-        $productDao = new ProductDao();
+        $dao = new ProductDao();
 
-        // Update the product in the database and catch the id
-        $id = $productDao->update($product);
+        // Update the product in the database and store the id
+        $id = $dao->update($product);
 
-        // Return the id
-        return ["id" => $id];
+        // Cast the id to an integer and return it to the controller
+        return ["id" => (int)$id];
     }
 );
 
+
+// Run the configured controller : 
+//          - Run the middlewares sequentially
+//          - Run the handler
 $app->run();
