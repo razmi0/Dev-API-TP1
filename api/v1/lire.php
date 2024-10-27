22687a6f8ab21650e715efea3124dda5cc6056a7
razmiO <thomas.cuesta31@gmail.com>
2024-10-27 18:20:49 +0100

    Refactor: Improve read one endpoint

--- api/v1/lire.php
+++ api/v1/lire.php
@@ -1,50 +1,95 @@
 <?php
 
+//  _____ _______ _______ _______ _______ _______ _______ _______
+// |                                                             |
+// |                  READ PRODUCT ENDPOINT                      |
+// |                                                             |
+// |          endpoint :  [GET] /api/v1.0/produit/list           |
+// |          file     :        /api/v1/lire.php                 |
+// |          goal     :  retrieve all products from db          |
+// |_____________________________________________________________|
+//
+
 require_once "../../Autoloader.php";
 
+/**
+ * 
+ * IMPORTS
+ * 
+ */
+
+// HTTP classes
 use HTTP\Request;
 use HTTP\Response;
+
+// Controller class
 use Controller\Controller;
+
+// Middleware class
 use Middleware\Middleware;
+
+// Model classes
 use Model\Constant;
 use Model\Dao\ProductDao;
 use Model\Entities\Product;
 use Model\Schema\Schema;
 
-// Create a new Controller object and start it with the request, response, middleware and handler
-$app = new Controller(
-    // We expect a GET request to this endpoint
-    new Request([
-        "methods" => ["GET"],
-        "endpoint" => "/api/v1/lire.php",
-    ]),
-    // We expect to return a 200 status code and a message
-    new Response([
-        "code" => 200,
-        "message" => "Produits récupérés avec succès",
-    ]),
-    // We will use 3 middlewares to check if the request is valid
+
+/**
+ * 
+ * INSTRUCTIONS
+ * 
+ */
+
+// The request object is configured for GET requests only on this endpoint
+$request = new Request([
+    "methods" => ["GET"],
+]);
+
+// The response object is configured to return a 200 status code and a successfull message
+$response = new Response([
+    "code" => 200,
+    "message" => "Produits récupérés avec succès",
+]);
+
+// Controller object
+$app = new Controller($request, $response);
+
+// Middleware object
+$app->setMiddleware(
     new Middleware(
-        // Function context : SCOPE : Middleware (class)
-        //                    OBJECT : new Middleware ($this)
+
+        // Context : Middleware scope and new Middleware object
+        // In this context, we access to : Request and Middleware objects
+
         function () {
+
             // Check if the request method is allowed else throw an error ( 405 Method Not Allowed )
             $this->checkAllowedMethods();
+
             // Check if the request body is a valid JSON else throw an error ( 400 Bad Request )
             $this->checkValidJson();
+
             // Check if the request body contains the expected data else throw an error ( 400 Bad Request )
             $this->checkExpectedData(new Schema(Constant::READ_ALL));
         }
-    ),
-    // Business logic at this endpoint : Get all products from the database
-    // Function context : SCOPE : Controller (class)
-    //                    OBJECT : new Controller ($this)
-    function () {
+    )
+);
 
-        // Get the limit from the query parameters ( string | null )
+// We set the handler for the controller to do the business logic
+// The handler is the core of the controller, it contains the business logic
+$app->setHandler(
+    function () {
+        // Get the limit from the query parameters
+        /**
+         * @var string $limitInQuery
+         */
         $limitInQuery = $this->request->getQueryParam("limit");
 
-        // Get the limit from the body ( integer | null )
+        // Get the limit from the body
+        /**
+         * @var integer $limitInBody
+         */
         $limitInBody = $this->request->getDecodedBody("limit");
 
         // Get the limit and cast it to an integer if it is from the query
@@ -56,10 +101,7 @@ $app = new Controller(
         // Create a new ProductDao object
         $dao = new ProductDao();
 
-        // Get all products from the database
-        /**
-         * @var Product[] $allProducts
-         */
+        // Get all products from the database (with a limit if provided)
         $allProducts = $dao->findAll($limit);
 
         // Map the products to an array
@@ -68,8 +110,9 @@ $app = new Controller(
         // Return the products array
         return ["products" => $productsArray];
     }
-
 );
 
-// Run the controller
+// Run the configured controller : 
+//          - Run the middlewares sequentially
+//          - Run the handler
 $app->run();
