d53f6e53cd5bf2d5f288a700f369fb1ad29afeca
razmiO <thomas.cuesta31@gmail.com>
2024-11-07 19:14:40 +0100

    Remove all schema definition

--- model/Constant.php
+++ model/Constant.php
@@ -9,106 +9,14 @@ use Model\Schema\Schema;
 /**
  * class Constant
  * 
- * All those constants emerged from the database
- * 
- * @property Schema CREATE_SCHEMA
- * @property Schema READ_ALL
- * @property Schema READ_ONE_SCHEMA
- * @property Schema DELETE_SCHEMA
- * @property Schema UPDATE_SCHEMA
- * @property Schema READ_MANY_SCHEMA
+ * All those constants emerged from the database and are use to validate the client json
  * 
  */
 class Constant
 {
-    private const NAME_REGEX = "/^[a-zA-Z0-9-'%,.:\/&()|; ]+$/";
-    private const DESCRIPTION_REGEX = "/^[a-zA-Z0-9-'%,.:\/&()|; ]+$/";
-    private const PRICE_REGEX = "/^[0-9.]+$/";
-    private const ID_REGEX = "/^[0-9]+$/";
+    public const NAME_REGEX = "/^[a-zA-Z0-9-'%,.:\/&()|; ]+$/";
+    public const DESCRIPTION_REGEX = "/^[a-zA-Z0-9-'%,.:\/&()|; ]+$/";
+    public const PRICE_REGEX = "/^[0-9.]+$/";
+    public const ID_REGEX = "/^[0-9]+$/";
     public const COLUMNS = ["id", "name", "description", "prix", "date_creation"];
-
-    /**
-     * This schema will be use in /api/v1/creer.php to validate client json
-     */
-    public const CREATE_SCHEMA = [
-        "name" => [
-            "type" => "string",
-            "range" => [1, 65],
-            "regex" => self::NAME_REGEX,
-            "required" => true
-        ],
-        "description" => [
-            "type" => "string",
-            "range" => [1, 65000],
-            "regex" => self::DESCRIPTION_REGEX,
-            "required" => true
-        ],
-        "prix" => [
-            "type" => "double",
-            "range" => [0, null],
-            "regex" => self::PRICE_REGEX,
-            "required" => true
-        ]
-    ];
-
-    public const READ_ALL = [
-        "limit" => [
-            "type" => "integer",
-            "required" => false,
-            "range" => [1, null],
-            "regex" => self::ID_REGEX
-        ]
-    ];
-
-    public const READ_ONE_SCHEMA = [
-        "id" => [
-            "type" => "integer",
-            "required" => false,
-            "range" => [1, null],
-            "regex" => self::ID_REGEX
-        ],
-    ];
-
-    public const DELETE_SCHEMA = [
-        "id" => [
-            "type" => "integer",
-            "required" => true,
-            "range" => [1, null],
-            "regex" => self::ID_REGEX
-        ]
-    ];
-
-    public const UPDATE_SCHEMA = [
-        "id" => [
-            "type" => "integer",
-            "required" => true,
-            "range" => [1, null],
-            "regex" => self::ID_REGEX
-        ],
-        "name" => [
-            "type" => "string",
-            "range" => [1, 65],
-            "regex" => self::NAME_REGEX,
-            "required" => false
-        ],
-        "description" => [
-            "type" => "string",
-            "range" => [1, 65000],
-            "regex" => self::DESCRIPTION_REGEX,
-            "required" => false
-        ],
-        "prix" => [
-            "type" => "double",
-            "range" => [0, null],
-            "regex" => self::PRICE_REGEX,
-            "required" => false
-        ]
-    ];
-
-    public const READ_MANY_SCHEMA = [
-        "id" => [
-            "type" => "integer[]",
-            "required" => false,
-        ]
-    ];
 }
