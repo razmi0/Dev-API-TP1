17f331ccba722d04aac7d3a88c9f57b634e315f7
razmiO <thomas.cuesta31@gmail.com>
2024-11-11 15:47:26 +0100

    Refactor Connection class and add setters for database name and table name

--- model/dao/Connection.php
+++ model/dao/Connection.php
@@ -7,7 +7,11 @@ use PDOException;
 use HTTP\Error;
 
 
-
+/**
+ * Class Connection
+ * 
+ * Has some presets for PDO connection and this project's database
+ */
 class Connection
 {
 
@@ -17,24 +21,33 @@ class Connection
     private $password = "";
     private $db_name = "db_labrest";
     private $table_name = "T_PRODUIT";
-    private ?Error $error = null;
 
 
     public function __construct()
     {
-        $this->error = new Error();
         try {
             $this->pdo = new PDO("mysql:host=$this->host;dbname=$this->db_name", $this->username, $this->password);
             $this->setPDOAttributes();
         } catch (PDOException $e) {
             error_log($e->getMessage());
-            throw $this->error->HTTP500("Service non disponible", [], "model/dao/Connection.php");
+            throw Error::HTTP500("Service non disponible", []);
         }
     }
+
+    public function setDbName($db_name)
+    {
+        $this->db_name = $db_name;
+    }
+
 
     public function getTableName()
     {
         return $this->table_name;
+    }
+
+    public function setTableName($table_name)
+    {
+        $this->table_name = $table_name;
     }
 
     public function getPDO()
