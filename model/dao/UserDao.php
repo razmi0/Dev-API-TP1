189fc5552b4b53f8910db02ccd388e17ea23c00a
razmiO <thomas.cuesta31@gmail.com>
2024-11-19 12:51:31 +0100

    Add UserDao class for managing user data in the database

---
+++ model/dao/UserDao.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace Model\Dao;
+
+use Exception;
+use Model\{Entity\User, Dao\Connection};
+use HTTP\Error as Error;
+use PDO;
+
+// T_USER columns : user_id, username, email, password_hash, created_at, updated_at
+
+class UserDao
+{
+    private ?PDO $pdo = null;
+    private ?Connection $connection = null;
+
+    public function __construct(Connection $connection)
+    {
+
+        $this->connection = $connection;
+        $this->pdo = $this->connection->getPDO();
+    }
+
+    /**
+     * @param User $user
+     * @throws Error 
+     * @return int $insertedId
+     */
+    public function create(User $user): int | false
+    {
+        try {
+
+            // sql query
+            // in the table T_TOKEN, we insert the token, token_hash and user_id (foreign key)
+            $sql = "INSERT INTO T_USER (username, email, password_hash) VALUES (:username, :email, :password_hash)";
+
+            // Prepare statement
+            $stmt = $this->pdo->prepare($sql);
+
+            // bind values
+            // --
+            $stmt->bindParam(":username", $user->getUsername());
+
+            $stmt->bindParam(":email", $user->getEmail());
+
+            $stmt->bindParam(":password_hash", $user->getPasswordHash());
+
+            $stmt->execute();
+
+            $insertedId = $this->pdo->lastInsertId();
+
+            // if the id is not set, (no user created) we return false
+            if (!$insertedId) {
+
+                return false;
+            }
+
+            // return the id of the inserted token
+            return (int)$insertedId;
+        } catch (Exception $e) {
+
+            error_log($e->getMessage());
+
+            throw Error::HTTP500("Erreur interne");
+        } finally {
+
+            $this->connection->closeConnection();
+        }
+    }
+
+    /**
+     * @param string $id
+     * @throws Error 
+     * @return User $user
+     */
+    public function find(string $field, string $value): User
+    {
+        try {
+            $sql = "SELECT * FROM T_USER WHERE $field = :value";
+
+            $stmt = $this->pdo->prepare($sql);
+
+            $stmt->bindParam(":value", $value);
+
+            $stmt->execute();
+
+            $data = $stmt->fetch();
+
+            if (!$data) {
+
+                Error::HTTP404("User non trouvé", ["token" => $value]);
+            }
+
+            return User::make($data);
+        } catch (Exception $e) {
+
+            error_log($e->getMessage());
+
+            throw Error::HTTP500("Erreur interne");
+        } finally {
+
+            $this->connection->closeConnection();
+        }
+    }
+}
