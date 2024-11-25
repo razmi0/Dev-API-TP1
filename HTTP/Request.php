8e1eda45da902a517f01934c184648a57a609699
razmiO <thomas.cuesta31@gmail.com>
2024-11-25 15:14:12 +0100

    Add request interface IRequest, improve readability and php docs, fix @throws now Exception, convert to singletonextending SingletonAbstract

--- HTTP/Request.php
+++ HTTP/Request.php
@@ -2,30 +2,44 @@
 
 namespace HTTP;
 
+use Exception;
 use HTTP\Error;
+use Utils\Console;
+use Utils\Patterns\{SingletonAbstract, ISingleton};
+
+interface IRequest extends ISingleton
+{
+    public function getRequestMethod(): string;
+    public function getDecodedData(): mixed;
+    public function getIsValidJson(): bool;
+    public function getJsonErrorMsg(): string;
+    public function getHasData(): bool;
+    public function getQueryParam($key): string|null|array;
+    public function getHeader(string $key): string|false;
+}
 
 /**
  * Class Request
  * 
- * @property string $request_method The request method
- * @property string $client_raw_json The raw body content of the request
- * @property array $client_decoded_data The decoded body content of the request
- * @property bool $has_data If the request has data
- * @property bool $is_valid_json If the request body is a valid json
- * @property string $json_error_msg The json error message
- * @property bool $has_query If the request has query parameters
- * @property array $query_params The query parameters
- * 
- * @method getRequestMethod()
- * @method getDecodedData()
- * @method getIsValidJson()
- * @method getJsonErrorMsg()
- * @method getHasData()
- * @method getQueryParam()
- * @method getHeader()
+ * This class is a singleton that handles the request data
+ * - **getInstance()** returns the instance of the class
+ * - **addAttribute()** adds an attribute to the request
+ * - **getAttribute()** gets an attribute from the request
+ * - **getHeader()** gets a header from the request
+ * - **getDomain()** gets the domain from the request
+ * - **getCookie()** gets a cookie from the request
+ * - **getDecodedData()** gets the decoded data from the request
+ * - **setDecodedBody()** sets the decoded data from the request
+ * - **getHasFormData()** checks if the request has form data
+ * - **getIsValidJson()** checks if the request has a valid JSON
+ * - **getJsonErrorMsg()** gets the JSON error message
+ * - **getHasData()** checks if the request has data
+ * - **getQueryParam()** gets a query parameter from the request
+ * - **getRequestMethod()** gets the request method
  */
-class Request
+class Request extends SingletonAbstract implements IRequest
 {
+    private static ?Request $instance = null;
     private string $request_method = "";
     private array $headers = [];
     private array $cookies = [];
@@ -41,46 +55,59 @@ class Request
     private bool $has_body_data = false;
     private bool $has_query = false;
 
-    public function __construct()
+    private function __construct()
     {
         try {
             $this->initializeRequest();
-        } catch (Error $e) {
+        } catch (Exception $e) {
+            Console::log($e->getMessage(), "IN REQUEST CLASS", [], 500);
             Error::HTTP500("Une erreur interne s'est produite", []);
         }
     }
 
+    public static function getInstance(): self
+    {
+        if (self::$instance === null) {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+
     private function initializeRequest(): void
     {
-        $this->request_method = $_SERVER["REQUEST_METHOD"] ?? "";
-        $this->domain = $_SERVER["HTTP_HOST"] ?? "";
-        $this->headers = getallheaders();
-        $this->cookies = $_COOKIE;
-        $this->client_raw_json = file_get_contents("php://input") ?? "";
-        $this->has_data = !empty($this->client_raw_json);
-        parse_str($_SERVER["QUERY_STRING"], $this->query_params);
-        $this->has_query = !empty($this->query_params);
+        $this->request_method = $_SERVER["REQUEST_METHOD"] ?? "";                  // Set request method
+        $this->domain = $_SERVER["HTTP_HOST"] ?? "";                               // Set request domain
+        $this->headers = getallheaders();                                          // Get all headers
+        $this->cookies = $_COOKIE;                                                 // Get all cookies
+        $this->client_raw_json = file_get_contents("php://input") ?? "";           // Get raw input
+        $this->has_data = !empty($this->client_raw_json);                          // Check if has data
+        parse_str($_SERVER["QUERY_STRING"], $this->query_params);                  // Parse query string
+        $this->has_query = !empty($this->query_params);                            // Check if has query
 
-        if (isset($_SERVER["CONTENT_TYPE"])) {
-            $this->parseRequestBody();
+        if (isset($_SERVER["CONTENT_TYPE"])) {                                     // Check content type
+            $this->parseRequestBody();                                             // Parse request body
         }
     }
 
     private function parseRequestBody(): void
     {
-        switch ($this->headers["Content-Type"]) {
-            case "application/x-www-form-urlencoded":
-                parse_str($this->client_raw_json, $this->client_decoded_data);
-                $this->has_form_data = !empty($this->client_decoded_data);
+        switch ($this->headers["Content-Type"]) {                                                           // Check content type
+            case "application/x-www-form-urlencoded":                                                       // Handle form data
+                parse_str($this->client_raw_json, $this->client_decoded_data);                              // Parse form data
+                $this->has_form_data = !empty($this->client_decoded_data);                                  // Check if form data
                 break;
 
-            case "application/json":
-                [$this->client_decoded_data, $this->is_valid_json, $this->json_error_msg] = self::safeDecode($this->client_raw_json);
-                $this->has_body_data = $this->is_valid_json && !empty($this->client_decoded_data);
+            case "application/json":                                                                        // Handle JSON data
+                [
+                    $this->client_decoded_data,
+                    $this->is_valid_json,
+                    $this->json_error_msg
+                ] = self::safeDecode($this->client_raw_json);                                               // Decode JSON
+                $this->has_body_data = $this->is_valid_json && !empty($this->client_decoded_data);          // Check if valid JSON
                 break;
 
-            default:
-                Error::HTTP415("Unsupported Media Type: " . $this->headers["Content-Type"]);
+            default:                                                                                        // Handle unsupported type
+                Error::HTTP415("Unsupported Media Type: " . $this->headers["Content-Type"]);                // Throw error
                 break;
         }
     }
