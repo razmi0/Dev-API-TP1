0ffa9af7182d1d1e97949fcbce6e3d3839bed741
razmiO <thomas.cuesta31@gmail.com>
2024-11-25 15:16:42 +0100

    Add Payload interface IPayload, improve readability and php docs, refactor param $config using new PayloadConfig utilitary class for better type hinting

--- HTTP/Payload.php
+++ HTTP/Payload.php
@@ -2,71 +2,89 @@
 
 namespace HTTP;
 
-const PROJECT_ROOT = __DIR__ . "/../";
+use HTTP\Config\PayloadConfig;
 
 require_once PROJECT_ROOT . 'vendor/autoload.php';
 
-class Payload
+interface IPayload
 {
-    private string $message = "";
-    private array $data = [];
-    private string $error = "";
+    public function getMessage(): string;
+    public function getData(): array;
+    public function getError(): string;
+    public function setMessage(string $message): self;
+    public function setData(array $data): self;
+    public function setError(string $error): self;
+    public function toJson(): string;
+    public function toArray(): array;
+}
 
-    public function __construct(array $config)
-    {
-        $this->message = $config["message"] ?? "";
-        $this->data = $config["data"] ?? [];
-        $this->error = $config["error"] ?? "";
-    }
+
+/**
+ * Payload class
+ * 
+ * This class is used to send a response to the client
+ * - **getMessage**: returns the message
+ * - **getData**: returns the data
+ * - **getError**: returns the error
+ * - **setMessage**: sets the message
+ * - **setData**: sets the data
+ * - **setError**: sets the error
+ * - **toJson**: returns the payload as a JSON string
+ * - **toArray**: returns the payload as an array
+ * - **__construct**: constructor($config)
+ */
+class Payload implements IPayload
+{
+    public function __construct(private PayloadConfig $config) {}
 
     public function getMessage(): string
     {
-        return $this->message;
+        return $this->config->get("message");
     }
 
     public function getData(): array
     {
-        return $this->data;
+        return $this->config->get("data");
     }
 
     public function getError(): string
     {
-        return $this->error;
+        return $this->config->get("error");
     }
 
     public function setMessage(string $message): self
     {
-        $this->message = $message;
+        $this->config->set("message", $message);
         return $this;
     }
 
     public function setData(array $data): self
     {
-        $this->data = $data;
+        $this->config->set("data", $data);
         return $this;
     }
 
     public function setError(string $error): self
     {
-        $this->error = $error;
+        $this->config->set("error", $error);
         return $this;
     }
 
     public function toJson(): string
     {
         return json_encode([
-            "message" => $this->message,
-            "data" => $this->data,
-            "error" => $this->error
+            "message" => $this->config->get("message"),
+            "data" => $this->config->get("data"),
+            "error" => $this->config->get("error")
         ]);
     }
 
     public function toArray(): array
     {
         return [
-            "message" => $this->message,
-            "data" => $this->data,
-            "error" => $this->error
+            "message" => $this->config->get("message"),
+            "data" => $this->config->get("data"),
+            "error" => $this->config->get("error")
         ];
     }
 }
